19129e9c9e728460ea4a428f7345b9cd15220fdd
muskan-fageria <muskanfageria31@gmail.com>
2026-09-20 08:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (31.70%) - LeetSync

---
+++ 1635-number-of-good-pairs/number-of-good-pairs.c
@@ -0,0 +1,9 @@
+int numIdenticalPairs(int* nums, int numsSize) {
+    int r=0;
+    for(int i=0; i<numsSize ; i++){
+        for( int j=i+1; j<numsSize; j++){
+            if (nums[i]==nums[j]) r=r+1;
+        }
+    }    
+    return r;
+}
