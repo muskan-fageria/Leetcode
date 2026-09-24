c144806ba9406ba4fe2a3c41d6ddcf799681ca31
muskan-fageria <muskanfageria31@gmail.com>
2026-09-24 17:40:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (80.64%) - LeetSync

--- 268-missing-number/missing-number.c
+++ 268-missing-number/missing-number.c
@@ -1,9 +1,8 @@
 int missingNumber(int* nums, int numsSize) {
-    int sum=0;
+    int sum=numsSize;
     int sum1=0;
-    for(int i=0; i<=numsSize; i++){
+    for(int i=0; i<numsSize; i++){
         sum=sum+i;
-        if (i==numsSize) continue;
         sum1=sum1+nums[i];
     }
     return sum-sum1;
